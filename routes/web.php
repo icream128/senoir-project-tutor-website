5f92534f47d89a76a59d45c71d161d659986d0f7
Areerat <jznwalkerz@gmail.com>
2017-09-06 05:02:25 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// tutor
 Route::get('/', function () {
     return view('welcome');
 });
@@ -38,6 +38,33 @@ Route::get('/createcourse', function () {
 Route::get('/favorite', function () {
     return view('favorite');
 });
-
+// learner
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/register', function () {
+    return view('register');
+});
+Route::get('/profile', function () {
+    return view('proflie');
+});
+Route::get('/history', function () {
+    return view('history');
+});
+Route::get('/mycourse', function () {
+    return view('mycourse');
+});
+Route::get('/showdetail', function () {
+    return view('showdetail');
+});
+Route::get('/createcourse', function () {
+    return view('createcourse');
+});
+Route::get('/favorite', function () {
+    return view('favorite');
+});
 
 
