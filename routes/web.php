421ba8cb1892a2b6c1cbda226e6fbfc51d2fbb2e
Areerat <jznwalkerz@gmail.com>
2017-09-05 20:05:09 +0700

    s

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/index', function () {
+    return view('welcome');
+});
+Route::get('/index', function () {
+    return view('welcome');
+});
+
